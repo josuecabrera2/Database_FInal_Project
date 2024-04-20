ef34b65b5268b0c4e5a61a13020f6faf3134dc4c
josuecabrera2 <77422060+josuecabrera2@users.noreply.github.com>
2024-04-19 21:21:55 -0400

    Update criminal_page.php

--- criminal_page.php
+++ criminal_page.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Criminal Details</title>
-    <link rel="stylesheet" href="css/second_page.css">
+    <link rel="stylesheet" href="css/criminal_page.css">
 </head>
 <body>
     <h1>Criminal Details</h1>
